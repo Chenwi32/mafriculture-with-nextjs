e183384c15fa1dd39fdb1f7f33270b2647d38d92
chenwi32 <chenwieugene.j@gmail.com>
2022-10-10 11:38:36 +0100

    populated the navigation bar

--- components/navigation.jsx
+++ components/navigation.jsx
@@ -1,21 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./compStyles/navigation.module.css";
 
 const Navigation = () => {
-    return (
-        <div>
-            <div className="container">
-                <div className="logo">
-                <Link href='/'>
-                    <Image src='/logo.png'
-                    width={100}
-                    height={50 } />
-                </Link>
-            </div>
-            </div>
-            
+  return (
+    <div>
+      <div className={`${styles.nav_container} container flex`}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image src="/logo.png" width={100} height={50} />
+          </Link>
         </div>
-    );
-}
+
+        <div className={styles.link}>
+          <Link href="/products">
+            <a className={styles.nav_link}>Products</a>
+          </Link>
+
+          <Link href="/products">
+            <a className={styles.nav_link}>Cart</a>
+          </Link>
+
+          <Link href="/products">
+            <a className={`${styles.nav_link} btn`}>Contact</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navigation;
